7fb347e7da9c17a6f2ea50f5608f185b870c9770
SulandPh <sulandsouhaib6@gmail.com>
2023-04-04 12:15:45 +0100

    forms login logout

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts.apps.AccountsConfig',
     'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -123,3 +124,6 @@ STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'blog/static') ]
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
